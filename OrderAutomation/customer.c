66d79acf5aa8bb0ccbdb2bd6a8ad3af801ec8da2
yavuzselimpek <yvz066@gmail.com>
2025-02-18 08:38:17 +0300

    Customer modülü tamamlandı

--- OrderAutomation/customer.c
+++ OrderAutomation/customer.c
@@ -60,11 +60,125 @@ void listCustomer()
 }
 void updateCustomer()
 {
+    system("cls");
+    printf("Customer list \n\n");
 
+    int counter=0,number,situation=0;
+
+    printf("%-10s%-30s%-10s%-30s%-20s%-30s\n","NUMBER","NAME-SURNAME","GENDER","ADDRESS","PHONE NUMBER","e-MAIL");
+
+    FILE *ptr = fopen("customers.dat", "r+b");//r+b read mode
+    while(fread( &c1, sizeof(customer), 1, ptr) != NULL)
+    {
+        printf("%-10d%-30s%-10c%-30s%-20s%-30s\n",c1.ID, c1.nameSurname, c1.gender, c1.address, c1.phoneNumber, c1.email);
+        counter++;
+    }
+    if(counter ==0)
+    {
+        system("cls");
+        printf("\n No customer registration! \n");
+    }
+    else
+    {
+        counter=0;
+        rewind(ptr);//We put the pointer at the beginning of the file
+        printf("\nEnter the customer number you want to update : ");scanf("%d",&number);
+            while(fread( &c1, sizeof(customer), 1, ptr) != NULL)
+            {
+                if(number == c1.ID)
+                {
+                        situation = 1;
+                        break;
+                }
+                counter++;
+            }
+            if(situation == 0)
+                printf("No customer record found with %d number! \n",number);
+            else
+            {
+                system("cls");
+                printf("%-10d%-30s%-10c%-30s%-20s%-30s\n",c1.ID, c1.nameSurname, c1.gender, c1.address, c1.phoneNumber, c1.email);
+                printf("Enter the data you want to update\n\n");
+                printf("Name - Surname : ");scanf(" %[^\n]s",c1.nameSurname);
+                printf("Gender         : ");scanf(" %[^\n]s",&c1.gender);
+                printf("Address        : ");scanf(" %[^\n]s",c1.address);
+                printf("Phone Number   : ");scanf(" %[^\n]s",c1.phoneNumber);
+                printf("Email          : ");scanf(" %[^\n]s",c1.email);
+                fseek(ptr , (counter) *sizeof(customer), 0);
+                fwrite( &c1, sizeof(customer), 1, ptr);
+                printf("Customer %d information updated \n",number);
+            }
+    }
+    fclose(ptr);
 }
 void deleteCustomer()
 {
+    system("cls");
+    printf("Customer list \n\n");
 
+    int counter=0,number,situation=0;
+
+
+    printf("%-10s%-30s%-10s%-30s%-20s%-30s\n","NUMBER","NAME-SURNAME","GENDER","ADDRESS","PHONE NUMBER","e-MAIL");
+
+    FILE *ptr = fopen("customers.dat", "r+b");//r+b read mode
+    while(fread( &c1, sizeof(customer), 1, ptr) != NULL)
+    {
+        printf("%-10d%-30s%-10c%-30s%-20s%-30s\n",c1.ID, c1.nameSurname, c1.gender, c1.address, c1.phoneNumber, c1.email);
+        counter++;
+    }
+    if(counter ==0)
+    {
+        system("cls");
+        printf("\n No customer registration! \n");
+    }
+    else
+    {
+        counter=0;
+        rewind(ptr);//We put the pointer at the beginning of the file
+        printf("\nEnter the customer number you want to delete : ");scanf("%d",&number);
+            while(fread( &c1, sizeof(customer), 1, ptr) != NULL)
+            {
+                if(number == c1.ID)
+                {
+                        situation = 1;
+                        break;
+                }
+                counter++;
+            }
+            if(situation == 0)
+                printf("No customer record found with %d number! \n",number);
+            else
+            {
+                system("cls");
+                printf("\n%-10d%-30s%-10c%-30s%-20s%-30s\n",c1.ID, c1.nameSurname, c1.gender, c1.address, c1.phoneNumber, c1.email);
+                char choice;
+                printf("Are you sure you want to delete customer record number %d? (Y/N): \n\n",number);scanf(" %c",&choice);
+                if(choice == 'y' || choice == 'Y')
+                {
+                    FILE *backupPtr = fopen("backup.dat", "w+b");
+                    while(fread( &c1, sizeof(customer), 1, ptr) != NULL)
+                    {
+                        if(number != c1.ID)
+                        {
+                            fwrite( &c1, sizeof(customer), 1, backupPtr);
+                        }
+                    }
+                    fclose(ptr);
+                    fclose(backupPtr);
+                    remove("customers.dat");
+                    rename("backup.dat","customers.dat");
+                    system("cls");
+                    printf("Customer record number %d was deleted\n",number);
+
+
+                }
+                else
+                    printf("\nRegistration was cancelled\n");
+
+            }
+    }
+    fclose(ptr);
 }
 
 int customerMenu()
